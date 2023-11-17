cb6969accc7e22a5178faaa6d7f35f23453ce10a
krishnababu18 <79692517+krishnababu18@users.noreply.github.com>
2023-11-17 11:55:06 -0600

    commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +23,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+ 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -44,6 +54,21 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-localmongoose");
+const accountSchema = new Schema({
+ username: String,
+ password: String
+});
+accountSchema.plugin(passportLocalMongoose);
+// We export the Schema to avoid attaching the model to the
+// default mongoose connection.
+module.exports = mongoose.model("Account"), accountSchema;
+
+
+
 require('dotenv').config();
 const connectionString =
 process.env.MONGO_CON
@@ -98,6 +123,24 @@ async function recreateDB(){
  }
  let reseed = true;
  if (reseed) { recreateDB();}
-
+ passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+ )
+ 
 
 module.exports = app;
